f15b76bb769d2de7aea7ac2ff96518f19716aadb
prathamesh19 <pratham.somnathe979@gmail.com>
2021-07-18 07:45:33 +0530

    first commit

---
+++ src/main/java/com/prathamesh/controller/JenkinsController.java
@@ -0,0 +1,11 @@
+package com.prathamesh.controller;
+
+public class JenkinsController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Everyone");
+	}
+
+}
---
+++ src/test/java/com/prathamesh/JenkinsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prathamesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
